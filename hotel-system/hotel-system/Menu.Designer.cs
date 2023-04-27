418b1c87594bf8a1f2e4028402ede7bd6058e0f3
ryanguilherme <ryanguilhermetbt@gmail.com>
2023-04-26 22:32:34 -0300

    iniciados menus laterais, botoes de atalho, menu lateral de hora e usuario (frontend)

--- hotel-system/hotel-system/Menu.Designer.cs
+++ hotel-system/hotel-system/Menu.Designer.cs
@@ -56,11 +56,26 @@ namespace hotel_system
             MenuSair = new ToolStripMenuItem();
             logoutToolStripMenuItem = new ToolStripMenuItem();
             TopPanel = new Panel();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
             RightPanel = new Panel();
+            label4 = new Label();
+            pictureBox2 = new PictureBox();
+            label5 = new Label();
+            label6 = new Label();
+            label3 = new Label();
+            pictureBox1 = new PictureBox();
             label2 = new Label();
             label1 = new Label();
             menuStrip1.SuspendLayout();
+            TopPanel.SuspendLayout();
             RightPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // menuStrip1
@@ -243,43 +258,203 @@ namespace hotel_system
             // 
             TopPanel.Anchor = AnchorStyles.Top | AnchorStyles.Left | AnchorStyles.Right;
             TopPanel.BackColor = SystemColors.AppWorkspace;
+            TopPanel.Controls.Add(button6);
+            TopPanel.Controls.Add(button5);
+            TopPanel.Controls.Add(button4);
+            TopPanel.Controls.Add(button3);
+            TopPanel.Controls.Add(button2);
+            TopPanel.Controls.Add(button1);
             TopPanel.Location = new Point(0, 27);
             TopPanel.Name = "TopPanel";
             TopPanel.Size = new Size(594, 75);
             TopPanel.TabIndex = 1;
             // 
+            // button6
+            // 
+            button6.BackColor = Color.Transparent;
+            button6.Cursor = Cursors.Hand;
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Image = (Image)resources.GetObject("button6.Image");
+            button6.Location = new Point(398, 3);
+            button6.Name = "button6";
+            button6.Size = new Size(73, 69);
+            button6.TabIndex = 5;
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.Transparent;
+            button5.Cursor = Cursors.Hand;
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Image = (Image)resources.GetObject("button5.Image");
+            button5.Location = new Point(161, 3);
+            button5.Name = "button5";
+            button5.Size = new Size(73, 69);
+            button5.TabIndex = 4;
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.Cursor = Cursors.Hand;
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Image = (Image)resources.GetObject("button4.Image");
+            button4.Location = new Point(319, 3);
+            button4.Name = "button4";
+            button4.Size = new Size(73, 69);
+            button4.TabIndex = 3;
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.Cursor = Cursors.Hand;
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Image = (Image)resources.GetObject("button3.Image");
+            button3.Location = new Point(240, 3);
+            button3.Name = "button3";
+            button3.Size = new Size(73, 69);
+            button3.TabIndex = 2;
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.Cursor = Cursors.Hand;
+            button2.FlatAppearance.BorderSize = 0;
+            button2.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Image = (Image)resources.GetObject("button2.Image");
+            button2.Location = new Point(82, 3);
+            button2.Name = "button2";
+            button2.Size = new Size(73, 69);
+            button2.TabIndex = 1;
+            button2.UseVisualStyleBackColor = false;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.Cursor = Cursors.Hand;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatAppearance.MouseOverBackColor = Color.Silver;
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Image = (Image)resources.GetObject("button1.Image");
+            button1.Location = new Point(3, 3);
+            button1.Name = "button1";
+            button1.Size = new Size(73, 69);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = false;
+            // 
             // RightPanel
             // 
             RightPanel.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Right;
-            RightPanel.BackColor = SystemColors.ScrollBar;
+            RightPanel.BackColor = SystemColors.WindowFrame;
+            RightPanel.Controls.Add(label4);
+            RightPanel.Controls.Add(pictureBox2);
+            RightPanel.Controls.Add(label5);
+            RightPanel.Controls.Add(label6);
+            RightPanel.Controls.Add(label3);
+            RightPanel.Controls.Add(pictureBox1);
             RightPanel.Controls.Add(label2);
             RightPanel.Controls.Add(label1);
             RightPanel.Location = new Point(600, 27);
             RightPanel.Name = "RightPanel";
-            RightPanel.Size = new Size(200, 411);
+            RightPanel.Size = new Size(200, 421);
             RightPanel.TabIndex = 2;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Georgia", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label4.ForeColor = SystemColors.ButtonHighlight;
+            label4.Location = new Point(-12, 173);
+            label4.Name = "label4";
+            label4.Size = new Size(223, 16);
+            label4.TabIndex = 7;
+            label4.Text = "________________________";
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.Image = (Image)resources.GetObject("pictureBox2.Image");
+            pictureBox2.Location = new Point(14, 109);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(45, 45);
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox2.TabIndex = 6;
+            pictureBox2.TabStop = false;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Franklin Gothic Medium", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label5.ForeColor = SystemColors.ButtonHighlight;
+            label5.Location = new Point(83, 147);
+            label5.Name = "label5";
+            label5.Size = new Size(75, 17);
+            label5.TabIndex = 5;
+            label5.Text = "Atendente";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Franklin Gothic Medium", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label6.ForeColor = SystemColors.ButtonHighlight;
+            label6.Location = new Point(65, 121);
+            label6.Name = "label6";
+            label6.Size = new Size(109, 17);
+            label6.TabIndex = 4;
+            label6.Text = "Ryan Guilherme";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Georgia", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label3.ForeColor = SystemColors.ButtonHighlight;
+            label3.Location = new Point(-12, 80);
+            label3.Name = "label3";
+            label3.Size = new Size(223, 16);
+            label3.TabIndex = 3;
+            label3.Text = "________________________";
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
+            pictureBox1.Location = new Point(14, 16);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(45, 45);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 2;
+            pictureBox1.TabStop = false;
             // 
             // label2
             // 
             label2.AutoSize = true;
             label2.Font = new Font("Franklin Gothic Medium", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
             label2.ForeColor = SystemColors.ButtonHighlight;
-            label2.Location = new Point(14, 45);
+            label2.Location = new Point(64, 58);
             label2.Name = "label2";
-            label2.Size = new Size(43, 17);
+            label2.Size = new Size(94, 17);
             label2.TabIndex = 1;
-            label2.Text = "Data:";
+            label2.Text = "26/04/2023";
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.Font = new Font("Franklin Gothic Medium", 9.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Font = new Font("Franklin Gothic Medium", 14.25F, FontStyle.Bold, GraphicsUnit.Point);
             label1.ForeColor = SystemColors.ButtonHighlight;
-            label1.Location = new Point(15, 18);
+            label1.Location = new Point(64, 28);
             label1.Name = "label1";
-            label1.Size = new Size(42, 17);
+            label1.Size = new Size(94, 24);
             label1.TabIndex = 0;
-            label1.Text = "Hora:";
+            label1.Text = "13:00:00";
             // 
             // menuForm
             // 
@@ -297,8 +472,11 @@ namespace hotel_system
             Load += menuForm_Load;
             menuStrip1.ResumeLayout(false);
             menuStrip1.PerformLayout();
+            TopPanel.ResumeLayout(false);
             RightPanel.ResumeLayout(false);
             RightPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -333,5 +511,17 @@ namespace hotel_system
         private Panel RightPanel;
         private Label label2;
         private Label label1;
+        private Button button1;
+        private Button button4;
+        private Button button3;
+        private Button button2;
+        private Button button6;
+        private Button button5;
+        private PictureBox pictureBox1;
+        private Label label3;
+        private Label label4;
+        private PictureBox pictureBox2;
+        private Label label5;
+        private Label label6;
     }
 }
